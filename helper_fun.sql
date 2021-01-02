0f8546943a06b6f5e4179abe9a53b50fd782673b
tomaztk <tomaz.kastrun@gmail.com>
2021-01-02 17:44:02 +0100

    minor change

--- helper_fun.sql
+++ helper_fun.sql
@@ -34,18 +34,74 @@ DECLARE @len INT
 PRINT @sql
 
 
--- works fine with single comments block
--- works fine with double (inline) comment block
--- works fine with multi-line comment blocks
+-- works fine with single comments block - OK 
+-- works fine with double (inline) comment block - NOK
+-- works fine with multi-line comment blocks - OK
 
-WHILE (CHARINDEX('/*',@sql)<>0)
+WHILE ( (CHARINDEX('/*',@sql) <> 0) OR (CHARINDEX('/*/*', @sql) <> 0) )
 BEGIN
-    SET @endPosition = charindex('*/',@sql)
-    SET @substrlen=len(substring(@sql,1,@endPosition-1))
-    SET @startPosition = @substrlen - charINDEX('*/',reverse(substring(@sql,1,@endPosition-1)))+1
+    SET @endPosition = CHARINDEX('*/',@sql)
+    SET @substrlen=LEN(SUBSTRING(@sql,1,@endPosition-1))
+    SET @startPosition = @substrlen - CHARINDEX('*/',REVERSE(SUBSTRING(@sql,1,@endPosition-1)))+1
     SET @commentLen = @endPosition - @startPosition
-    SET @comment = substring(@sql,@startPosition-1,@commentLen+3 )
+    SET @comment = SUBSTRING(@sql,@startPosition-1,@commentLen+3 )
     SET @sql = REPLACE(@sql,@comment,CHAR(13))
 END
 
 PRINT @sql
+
+
+DECLARE @sql VARCHAR(500) = '-- Query
+SELECT top 10 
+ [name]
+,object_id
+--,principal_id
+--,schema_did
+,schema_id
+from sys.tables'
+
+DECLARE @comment VARCHAR(500)
+DECLARE @endPosition INT
+DECLARE @startPosition INT
+DECLARE @commentLen INT
+DECLARE @substrlen INT
+DECLARE @len INT
+DECLARE @StartPos INT
+
+SELECT CHARINDEX(CHAR(10),@sql)
+,CHARINDEX('--',@sql)
+,Len(@sql)
+
+/*
+WHILE  (CHARINDEX('--',@sql) <> 0)
+BEGIN
+    SET @startPos = CHARINDEX('--', @sql)
+	SET @endPosition = CHARINDEX(CHAR(10),@sql)
+    SET @commentLen = @endPosition - @startPos
+	SET @SQL = SUBSTRING(@SQL,@endPosition-1,LEN(@SQL))
+END
+
+PRINT @sql
+
+*/
+
+DECLARE  @comm TABLE (pos INT)
+DECLARE @pos INT
+DECLARE @oldpos INT = 0
+SELECT @pos = CHARINDEX('--',@sql) 
+DECLARE @REP VARCHAR(MAX) = ''
+
+WHILE @pos > 0 AND @oldpos <> @pos
+BEGIN
+   INSERT INTO @comm VALUES(@pos)
+   SELECT @oldpos=@pos
+   SELECT @pos=CHARINDEX('--',Substring(@sql,@pos + 2,len(@sql))) + @pos
+   SET @sql = REPLACE(@sql, @rep, '')
+   print(@sql)
+   SET @REP = SUBSTRING(@SQL, @oldpos, CHARINDEX(CHAR(10), @sql))
+   --print (charindex(char(10),@sql))
+
+END
+
+SELECT * FROM @comm
+PRINT @SQL
