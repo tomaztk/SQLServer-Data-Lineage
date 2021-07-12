42e28de2db8a8aeb05abec6cc5d1e6b60ea9717b
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-07-12 22:52:58 +0200

    adding improved function

---
+++ v2_strip_comments.sql
@@ -0,0 +1,113 @@
+USE QL;
+
+DECLARE @sp_text varchar(8000) = ''
+DECLARE @sp_text_row varchar(8000)
+DECLARE @sp_no_comment varchar(8000) = ''
+DECLARE @c char(1)
+DECLARE @comment_count INT = 0
+
+
+SELECT @sp_text = @sp_text + CASE 
+								WHEN LEN(@sp_text) > 0 THEN '\n' 
+								ELSE '' END + query_txt
+FROM dbo.SQL_query_table
+
+PRINT @sp_text
+
+DECLARE @i INT  = 1
+DECLARE @rowcount INT = (SELECT LEN(@sp_text))
+PRINT @rowcount
+
+WHILE (@i <= @rowcount) 
+	BEGIN
+		 IF SUBSTRING(@sp_text,@i,2) = '/*'
+			BEGIN
+				-- SELECT (SUBSTRING(@sp_text,@i,2)) -- Uncomment or Delete
+				SELECT @comment_count = @comment_count + 1
+			END
+		 ELSE IF SUBSTRING(@sp_text,@i,2) = '*/'  
+			BEGIN
+				SELECT @comment_count = @comment_count - 1  
+				-- SELECT @comment_count -- Uncomment or Delete
+			END
+		 ELSE IF @comment_count = 0
+			SELECT @sp_no_comment = @sp_no_comment + SUBSTRING(@sp_text,@i,1)
+
+		 IF SUBSTRING(@sp_text,@i,2) = '*/' 
+		  SELECT @i = @i + 2
+		 ELSE
+		  SELECT @i = @i + 1
+	END
+
+
+WHILE (@i <= @rowcount) 
+	BEGIN
+		 IF SUBSTRING(@sp_text,@i,4) = '/*/*'
+			BEGIN
+				SELECT @comment_count = @comment_count + 2
+			END
+		 ELSE IF SUBSTRING(@sp_text,@i,4) = '*/*/'  
+			BEGIN
+				SELECT @comment_count = @comment_count - 2 
+			END
+		 ELSE IF @comment_count = 0
+			SELECT @sp_no_comment = @sp_no_comment + SUBSTRING(@sp_text,@i,1)
+
+		 IF SUBSTRING(@sp_text,@i,4) = '*/*/' 
+		  SELECT @i = @i + 2
+		 ELSE
+		  SELECT @i = @i + 1
+	END
+
+DROP TABLE IF EXISTS  #tbl_sp_no_comments
+CREATE TABLE #tbl_sp_no_comments (
+			 rn int identity(1,1)
+			,sp_text varchar(8000)
+			)
+
+
+WHILE (LEN(@sp_no_comment) > 0)
+	BEGIN
+		INSERT INTO  #tbl_sp_no_comments (sp_text)
+		SELECT SUBSTRING( @sp_no_comment, 0, CHARINDEX('\n', @sp_no_comment))
+		SELECT @sp_no_comment = SUBSTRING(@sp_no_comment, CHARINDEX('\n',@sp_no_comment) + 2, LEN(@sp_no_comment))
+	END
+
+SELECT * FROM #tbl_sp_no_comments
+
+
+DROP TABLE IF EXISTS  #tbl_sp_no_comments_fin
+CREATE TABLE #tbl_sp_no_comments_fin (rn_orig int identity(1,1), rn INT, sp_text_fin varchar(8000))
+
+
+DECLARE @nofRows INT =  (SELECT COUNT(*) FROM #tbl_sp_no_comments)
+PRINT @nofRows
+
+WHILE (@nofRows > 0)
+BEGIN
+
+	DECLARE @LastLB INT = 0
+	DECLARE @Com INT = 0 
+	SET @Com = (SELECT CHARINDEX('--', sp_text,@com) FROM #tbl_sp_no_comments WHERE rn = @nofRows)
+
+	SET @LastLB = (SELECT CHARINDEX(CHAR(10), sp_text, @LastLB) FROM #tbl_sp_no_comments WHERE rn = @nofRows)
+
+
+	
+	--INSERT INTO #tbl_sp_no_comments_fin VALUES (@nofRows,  )
+
+	SELECT 
+		rn
+		,sp_text
+		,@Com AS StartCom
+		,@LastLB AS endCom
+	FROM #tbl_sp_no_comments
+	WHERE 
+		rn = @nofRows
+
+
+	SET @nofRows = @nofRows - 1
+
+END
+
+
