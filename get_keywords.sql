17c0219011d9d663509934696a27aeb03107efbc
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-09-19 07:31:07 +0200

    update

--- get_keywords.sql
+++ get_keywords.sql
@@ -12,19 +12,22 @@ union all select 'GROUP BY'
 
 DECLARE @sqlStatement AS VARCHAR(200)
 SET @sqlStatement ='
-SELECT top 10 
+SELECT  
  [name]
 ,object_id
 ,schema_id
-from sys.tables
-join (select * from sys.objects)
+FROM sys.tables AS t
+JOIN (select * from sys.objects where type = ''IT'') AS O
+ON t.object_id = o.object_Id
+WHERE
+	t.name like ''test%''
 '
 
 DECLARE @search_results TABLE (
 		 id int identity(1,1)
-		,word varchar(100)
-		,pos INT
-		,posEND INT)
+		,Res_word varchar(100)
+		,pos_start INT
+		,pos_end INT)
 
 
 DECLARE @ii INT = 1
@@ -35,18 +38,17 @@ WHILE @ii <= @nof_words
 BEGIN
 	
 	  DECLARE @i INT = 1
-	  DECLARE @Search_String AS VARCHAR(100)  
-	  SET @Search_String = (SELECT word from @reserved_words where id=@ii)
-	  PRINT @search_string
-	  DECLARE @stringLen int = LEN(@Search_String)
+	  DECLARE @search_res_word AS VARCHAR(100)  
+	  SET @search_res_word = (SELECT word from @reserved_words where id=@ii)
+	  DECLARE @word_len int = LEN(@search_res_word)
 		
 	  WHILE @i < LEN(@sqlStatement)
 		BEGIN
 
-		  IF  (SUBSTRING(@sqlStatement,@i,@stringLen) = @Search_String)
+		  IF  (SUBSTRING(@sqlStatement,@i,@word_len) = @search_res_word)
 		      BEGIN
-			   insert into @search_results (word, pos, posEND)
-			   select @Search_String, @i, @i+LEN(@Search_String)
+			   INSERT INTO @search_results (res_word, pos_start, pos_end)
+			   SELECT @search_res_word, @i, @i+LEN(@search_res_word)
 		      END
 		   SET @i = @i + 1		END
 
@@ -56,3 +58,15 @@ END
 
 
 select * from @search_results
+
+
+DECLARE @lin_table TABLE (
+		 id int identity(1,1)
+		 ,[level] INT
+		,[schema] varchar(100)
+		,[table] VARCHAR(100)
+		)
+
+-- name of the tables / objects
+-- after from statement, if not followed by (
+
