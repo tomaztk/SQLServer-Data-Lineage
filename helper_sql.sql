87c148d805d563387ac3f296d16df11715ea40cb
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-01-22 21:33:10 +0100

    update

--- helper_sql.sql
+++ helper_sql.sql
@@ -14,13 +14,15 @@ CREATE TABLE dbo.Departments
 (
  DepartmentId   INT IDENTITY(1,1) NOT NULL
 ,DepartmentName VARCHAR(20)
-)
+);
+GO
 
 INSERT INTO dbo.Departments VALUES ('SQL Server') 
 INSERT INTO dbo.Departments VALUES ('Power BI') 
 INSERT INTO dbo.Departments VALUES ('DBA 101') 
 INSERT INTO dbo.Departments VALUES ('Azure SQL Database') 
-INSERT INTO dbo.Departments VALUES ('Machine Learning')
+INSERT INTO dbo.Departments VALUES ('Machine Learning');
+GO
 
 
 DROP TABLE IF EXISTS dbo.Students
@@ -31,7 +33,8 @@ CREATE TABLE dbo.Students
 ,Surname VARCHAR(40) NOT NULL
 ,DateOfBirth DATE NOT NULL
 ,DepartmentId INT NOT NULL
-) 
+);
+GO
 
 INSERT INTO dbo.Students VALUES( 'Markus', 'Miller', '1980-05-31', 1)
 INSERT INTO dbo.Students VALUES( 'Mike', 'Patton', '1968-02-02', 1)
@@ -39,7 +42,8 @@ INSERT INTO dbo.Students VALUES( 'Stone', 'Gossard', '1962-07-29', 2)
 INSERT INTO dbo.Students VALUES( 'Geoffrey', 'Wilson', '1977-01-21', 3)
 INSERT INTO dbo.Students VALUES( 'Mike', 'McCready', '1968-10-10', 4)
 INSERT INTO dbo.Students VALUES( 'John', 'Smith', '1975-01-01',4)
-INSERT INTO dbo.Students VALUES( 'Abigale', 'Wail', '1955-01-01',3)
+INSERT INTO dbo.Students VALUES( 'Abigale', 'Wail', '1955-01-01',3);
+GO
 
 
 
