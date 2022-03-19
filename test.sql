bd51d5fe02f88b4cfaecc400ce7edbd0ede7f128
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-03-19 22:24:41 +0100

    update

--- test.sql
+++ test.sql
@@ -26,9 +26,9 @@ FROM [AdventureWorks2014].sales.[SalesPerson] s
 
 
 
-DECLARE @sqlStatement3 AS VARCHAR(2000)
+DECLARE @stmt AS NVARCHAR(2000)
 
-SET @sqlStatement3 ='SELECT
+SET @stmt ='SELECT
  d1.name
 ,d1.database_id
 ,(select GETDATE()) as dd
@@ -47,48 +47,89 @@ on x.name = d1.name'
 query info
 */
 
-declare @q_len int = DATALEngth(@stmt)
+declare @q_len int = DATALENGTH(@stmt)
 declare @i int = 1
 
-declare @Results TABLE (SearchWord VARCHAR(100), i int, j int, sqlobject nvarchar(100))
+declare @Results TABLE (SearchWord VARCHAR(100),
+    i int,
+    j int,
+    sqlobject nvarchar(100))
 
 
 declare @res_words nvarchar(1000) = 'from,join,where,exists,with,apply,select'
 
-DECLARE @reserved_words_tables TABLE (id int identity(1,1), word varchar(100))
-INSERT INTO @reserved_words_tables (word)
-SELECT [VALUE] FROM string_split(@res_words, ',')
+DECLARE @reserved_words_tables TABLE (id int identity(1,1),
+    word varchar(100))
+INSERT INTO @reserved_words_tables
+    (word)
+SELECT [VALUE]
+FROM string_split(@res_words, ',')
 
-DECLARE @result_table TABLE (rword varchar(100), position int, rwordlen int, tableName varchar(1000))
+DECLARE @result_table TABLE (rword varchar(100),
+    position int,
+    rwordlen int,
+    tableName varchar(1000))
 
 
-while @i <= (select max(id) from @reserved_words_tables)
+while @i <= (select max(id)
+from @reserved_words_tables)
 begin
-	DECLARE @int_word nvarchar(100) = (select word from @reserved_words_tables where id = @i)
-	declare @ii int = 1
-	while @ii < @q_len
+    DECLARE @int_word nvarchar(100) = (select word
+    from @reserved_words_tables
+    where id = @i)
+    declare @ii int = 1
+    while @ii < @q_len
 	begin
-		if substring(@stmt,@ii,LEN(@int_word)) = @int_word
+        if substring(@stmt,@ii,LEN(@int_word)) = @int_word
 		begin
-					INsert into  @result_table
-					 SELECT 
-					 @int_word
-					  ,@ii
-					  ,LEN(@int_word)
+            INSERT into  @result_table
+            SELECT
+                @int_word
+					  , @ii
+					  , LEN(@int_word)
 
-					    ,REPLACE(REPLACE(SUBSTRING(
+					    , REPLACE(REPLACE(SUBSTRING(
 				       TRIM(SUBSTRING(@stmt, @ii+LEN(@int_word), @q_len))
 						,1
 						,PATINDEX('% %', TRIM(SUBSTRING(@stmt, @ii+LEN(@int_word), @q_len)))), ')',''),'(','') as tableName
-			END
-			SET @ii += 1		
-		END
-		
-	 SET @i = @i + 1 
+        END
+        SET @ii += 1
+    END
+
+    SET @i = @i + 1
 
 END
 
+DROP TABLE IF EXISTS dbo.TK_test;
 
-select *from @result_table
+select
+    row_number() over (ORDER BY position ASC) as RN
+, * 
+, 0 AS LVL
+into TK_test
+from @result_table
 order by position asc
 -- last from that can be before join or select or exists
+
+
+
+IF (SELECT rword
+from TK_TEST
+where rn = 1 ) = 'SELECT'
+BEGIN
+    -- select first
+    UPDATE TK_TEST
+        SET lvl = 1
+    WHERE 
+        RN = 1
+END
+ELSE BEGIN
+    -- SELECT NOT first
+    DECLARE @res_words2_order NVARCHAR(1000) = 'select,delete,insert,drop,create,truncate,exec,execute'
+    SELECT 'No'
+END
+
+
+SELECT *
+FROM TK_TEST
+order by rn asc
