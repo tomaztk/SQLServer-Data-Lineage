ba84bca70959aed4f70b62282688f76abf384f81
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-06-09 14:45:23 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,6 +1,32 @@
-use AdventureWorks2014;
+USE Lineage;
+GO
 
-declare @stmt nvarchar(4000) = '
+CREATE OR ALTER FUNCTION dbo.fn_removelistChars
+/*
+Desc: Function for removing list of unwanted characters
+Created: 06.JUN.2022
+Author: TK
+Usage:
+	SELECT dbo.fn_removelistChars('Tol~99""''''j\e.j/e[,t&eks]t,ki')
+
+*/
+(
+	@txt AS VARCHAR(max)
+)
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @list VARCHAR(200) = '%[^a-zA-Z0-9+@#\/%=_?!:.''-]%'
+    WHILE PATINDEX(@list,@txt) > 0
+		SET @txt = REPLACE(@txt,SUBSTRING(@txt,PATINDEX(@list,@txt),1),'')
+RETURN @txt
+
+END;
+GO   
+
+
+
+declare @stmt VARCHAR(8000) = '
 
 SELECT 
     s.[BusinessEntityID]
@@ -9,157 +35,59 @@ SELECT
     ,p.[MiddleName]
     ,p.[LastName]
     ,p.[Suffix]
-    ,e.[JobTitle]
+    ,e.[JobTitle] as imeSluzbe
     ,p.[EmailPromotion]
     ,s.[SalesQuota]
     ,s.[SalesYTD]
     ,s.[SalesLastYear]
-	,( SELECT GETDATE()) AS DateNow
-	,(select count(*)  FROM [AdventureWorks2014].sales.[SalesPerson]) as totalSales
+	,( SELECT GETDATE() ) AS DateNow
+	,( select count(*)  FROM [AdventureWorks2014].sales.[SalesPerson] ) as totalSales
 
 FROM [AdventureWorks2014].sales.[SalesPerson] s
-    INNER JOIN [AdventureWorks2014].[HumanResources].[Employee] e 
+    LEFT JOIN [AdventureWorks2014].[HumanResources].[Employee] e 
     ON e.[BusinessEntityID] = s.[BusinessEntityID]
-	INNER JOIN [AdventureWorks2014].[Person].[Person] p
+	INNER JOIN [AdventureWorks2014].[Person].[Person] AS p
 	ON p.[BusinessEntityID] = s.[BusinessEntityID]
+
 '
 
 
 
-DECLARE @stmt AS NVARCHAR(2000)
-
-SET @stmt ='SELECT
- d1.name
-,d1.database_id
-,(select GETDATE()) as dd
-,X.compatibility_level 
-
-from sys.databases as d1
-join (select * from sys.databases) as x
-on x.name = d1.name'
 
 
-
--- exec sp_executesql @stmt
-
-
-/*
-query info
-*/
-
-declare @q_len int = DATALENGTH(@stmt)
-declare @i int = 1
-
-declare @Results TABLE (SearchWord VARCHAR(100),
-    i int,
-    j int,
-    sqlobject nvarchar(100))
-
-
-declare @res_words nvarchar(1000) = 'from,join,where,exists,with,apply,select'
-
-DECLARE @reserved_words_tables TABLE (id int identity(1,1),
-    word varchar(100))
-INSERT INTO @reserved_words_tables
-    (word)
-SELECT [VALUE]
-FROM string_split(@res_words, ',')
-
-DECLARE @result_table TABLE (rword varchar(100),
-    position int,
-    rwordlen int,
-    tableName varchar(1000))
-
-
-while @i <= (select max(id)
-from @reserved_words_tables)
-begin
-    DECLARE @int_word nvarchar(100) = (select word
-    from @reserved_words_tables
-    where id = @i)
-    declare @ii int = 1
-    while @ii < @q_len
-	begin
-        if substring(@stmt,@ii,LEN(@int_word)) = @int_word
-		begin
-            INSERT into  @result_table
-            SELECT
-                @int_word
-					  , @ii
-					  , LEN(@int_word)
-
-					    , REPLACE(REPLACE(SUBSTRING(
-				       TRIM(SUBSTRING(@stmt, @ii+LEN(@int_word), @q_len))
-						,1
-						,PATINDEX('% %', TRIM(SUBSTRING(@stmt, @ii+LEN(@int_word), @q_len)))), ')',''),'(','') as tableName
-        END
-        SET @ii += 1
-    END
-
-    SET @i = @i + 1
-
-END
-
-DROP TABLE IF EXISTS dbo.TK_test;
-
-select
-    row_number() over (ORDER BY position ASC) as RN
-, * 
-, 0 AS LVL
-into TK_test
-from @result_table
-order by position asc
--- last from that can be before join or select or exists
-
-
-
-IF (SELECT rword
-from TK_TEST
-where rn = 1 ) = 'SELECT'
-BEGIN
-    -- select first
-    UPDATE TK_TEST
-        SET lvl = 1
-    WHERE 
-        RN = 1
-END
-ELSE BEGIN
-    -- SELECT NOT first
-    DECLARE @res_words2_order NVARCHAR(1000) = 'select,delete,insert,drop,create,truncate,exec,execute'
-    SELECT 'No'
-END
-
-
-SELECT *
-FROM TK_TEST
-order by rn asc
-
-
-
----- test
 DROP TABLE IF EXISTS TK_TEST2
-declare @st nvarchar(200) = '
 
-select  a, b, c from  dbo.tabela join  dbo.tabla as ta 
-on tabela.a = tabla.b  
-where a > b and c = 201;'
-
-
-DECLARE @st2 NVARCHAR(200)
-SET @st2 = REPLACE(REPLACE(@st, CHAR(13), ''), CHAR(10), '')
+DECLARE @stmt2 NVARCHAR(4000)
+SET @stmt2 = REPLACE(REPLACE(@stmt, CHAR(13), ' '), CHAR(10), ' ')
 
 
 select 
 TRIM(REPLACE(value, ' ','')) as val
+,dbo.fn_removelistChars(value) as val_f
 ,row_number() over (ORDER BY (SELECT 1)) as rn
 INTO TK_TEST2
-from string_split(REPLACE(@st, CHAR(13), ' '), ' ' )
+from string_split(REPLACE(@stmt2, CHAR(13), ' '), ' ' )
 WHERE
     REPLACE(value, ' ','') <> ' ' 
 OR REPLACE(value, ' ','') <> ' '
 
 
-SELECT * FROM TK_TEST2
+
+
+SELECT 
+*
+,case when val like '%(%' then 1 else 0 end as predok
+,case when val like '%)%' then 1 else 0 end as zak
+,case when val like '%select%' then 1 else 0 end as select_
+,case when val like '%FROM%' then 1 else 0 end as from_
+,case when val like '%join%' then 1 else 0 end as join_
+,case when val like '%where%' then 1 else 0 end as where_
+FROM TK_TEST2
+
+
+
+
+
 
 -- @token = @tokenen
 -- @token_i = @tokenen_i
@@ -205,7 +133,7 @@ BEGIN
 						
 								SET @ttok = ' ' + @token + ' as ('
 								--IF (@ttok NOT IN (SELECT @token))
-								 IF (@ttok NOT IN (SELECT @st2))
+								 IF (@ttok NOT IN (SELECT @stmt2))
 									INSERT INTO @table (tik, tok, order_)
 									SELECT @token_i, @token, @order
 
@@ -228,74 +156,6 @@ BEGIN
 			SET @i_row = @i_row + 1
 END
 
+-- Final results
 SELECT * FROM @table
-
-/*
-
-DECLARE @prev_word VARCHAR(1000), @int_word VARCHAR(1000)
-DEclare @row_commands nvarchar(1000) = 'select,delete,insert,drop,create,select,truncate,exec,execute'
-declare @result table (wordd varchar(100), intt varchar(100), i_row varchar(100))
-
-WHILE @max_row >= @i_row
-BEGIN
-    DECLARE @row VARCHAR(1000) = (SELECT val FROM TK_TEST2 WHERE rn = @i_row)
-    IF lower(@row) in (SELECT replace(trim(lower(value)), ' ','') from string_split(@row_commands, ','))
-         IF @row = 'select'
-         BEGIN
-            SET @int_word = 'SELECT'
-           SELECT @int_word
-           END
-        ELSE
-            SET @row = @prev_word
-            IF LOWER(@int_word) NOT IN (SELECT replace(trim(lower(value)), ' ','') from string_split(@row_commands, ','))
-            BEGIN
-                IF LOWER(@prev_word) = 'INTO'
-                SET @int_word = 'SELECT INTO'
-                    IF LOWER (@prev_word) NOT LIKE '%#%' OR LOWER(@prev_word)  NOT LIKE '%@%'
-                    SET @prev_word = ' ' + @prev_word + ' as ('
-
-		    IF @int NOT IN (SELECT replace(trim(lower(value)), ' ','') from string_split(@row_commands, ','))
-			     INSERT INTO @result
-			     SELECT 
-			     	@word, @int, @i_row
-				
-				SET @i_row += 1
-				SET @int = REPLACE(@int,  ' ', '')
-            END
-        
-    ELSE
-    BEGIN
-        SELECT 'no'
-    END
-    SET @i_row = @i_row + 1
-END
-
-*/
-
-
-
-----
-
-CREATE OR ALTER FUNCTION dbo.fn_removelistChars
-/*
-Desc: Function for removing list of unwanted characters
-Created: 06.JUN.2022
-Author: TK
-Usage:
-	SELECT dbo.fn_removelistChars('Tol~99""''''j\e.j/e[,t&eks]t,ki')
-
-*/
-(
-	@txt AS VARCHAR(max)
-)
-RETURNS VARCHAR(MAX)
-AS
-BEGIN
-	DECLARE @list VARCHAR(200) = '%[^a-zA-Z0-9+@#\/%=_?!:.''-]%'
-    WHILE PATINDEX(@list,@txt) > 0
-		SET @txt = REPLACE(@txt,SUBSTRING(@txt,PATINDEX(@list,@txt),1),'')
-RETURN @txt
-
-END;
-GO   
 
