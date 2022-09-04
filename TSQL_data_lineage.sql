05c08e4887ab263940f95fd22718aa03c251a715
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-09-04 16:07:52 +0200

    update script

--- TSQL_data_lineage.sql
+++ TSQL_data_lineage.sql
@@ -10,9 +10,11 @@ GO
 
 CREATE OR ALTER FUNCTION dbo.fn_removelistChars
 /*
-Desc: Function for removing list of unwanted characters
+Author: Tomaz Kastrun
 Created: 06.JUN.2022
-Author: TK
+
+Desc: Function for removing list of unwanted characters
+
 Usage:
 	SELECT dbo.fn_removelistChars('Tol~99""''''j\e.j/e[,t&eks]t,ki')
 
@@ -33,7 +35,7 @@ GO
 
 
 
-CREATE OR ALTER dbo.TSQL_data_lineage 
+CREATE OR ALTER PROCEDURE dbo.TSQL_data_lineage 
 /*
 Author: Tomaz Kastrun
 Date: August 2022
@@ -65,19 +67,43 @@ BEGIN
 
 
 
-    DROP TABLE IF EXISTS dbo.SQL_query_table;
+DROP TABLE IF EXISTS dbo.SQL_query_table
 
-    CREATE TABLE dbo.SQL_query_table (
-        id INT IDENTITY(1,1) NOT NULL
-        ,query_txt NVARCHAR(4000)
-    )
+CREATE TABLE dbo.SQL_query_table (
+    id INT IDENTITY(1,1) NOT NULL
+    ,query_txt NVARCHAR(4000)
+)
 
-    INSERT INTO dbo.SQL_query_table
-    EXEC sp_helptext  
-        @objname =  @InputQuery
+    -- Breaks the procedure into lines with linebreak
+    -- INSERT INTO dbo.SQL_query_table
+    -- EXEC sp_helptext  
+    --     @objname =  @InputQuery
+
+        -- Breaks the query into lines with linebreak
+            DECLARE @MAX_nof_break INT = (select len(@InputQuery) - len(replace(@InputQuery, CHAR(10), '')))
+            DECLARE @start_nof_break INT = 1
+            declare @iq2 NVARCHAR(max) = @InputQuery
+
+            declare @max_len int = (SELECT len(@InputQuery))
+            declare @start_pos int = 0
+            declare @br_pos int = 0
 
 
 
+            while (@MAX_nof_break >= @start_nof_break)
+            BEGIN
+
+                SET @br_pos = (SELECT charindex( char(10), @iq2) )
+                INSERT INTO dbo.SQL_query_table(query_txt)
+                    SELECT  substring(@InputQuery,@start_pos, @br_pos )
+                
+                SET @start_pos = @start_pos + @br_pos  
+                SET @iq2 = SUBSTRING(@InputQuery, @start_pos, @max_len)
+                SET @start_nof_break = @start_nof_break + 1
+            END
+
+
+    --- STart removing comments
     DECLARE @proc_text varchar(MAX) = ''
     DECLARE @proc_text_row varchar(MAX)
     DECLARE @proc_no_comment varchar(MAX) = ''
@@ -196,6 +222,120 @@ BEGIN
 ******************************** */
 
 
+DROP TABLE IF EXISTS TK_TEST2
+
+
+DECLARE @stmt NVARCHAR(MAX) = (
+
+    SELECT 
+        sp_text_fin  
+    FROM #tbl_sp_no_comments_fin
+    WHERE	
+        DATALENGTH(sp_text_fin) > 0 
+    AND LEN(sp_text_fin) > 0
+)
+
+DECLARE @stmt2 NVARCHAR(4000) 
+SET @stmt2 = REPLACE(REPLACE(@stmt, CHAR(13), ' '), CHAR(10), ' ')
+
+
+select 
+TRIM(REPLACE(value, ' ','')) as val
+,dbo.fn_removelistChars(value) as val_f
+,row_number() over (ORDER BY (SELECT 1)) as rn
+INTO TK_TEST2
+from string_split(REPLACE(@stmt2, CHAR(13), ' '), ' ' )
+WHERE
+    REPLACE(value, ' ','') <> ' ' 
+OR REPLACE(value, ' ','') <> ' '
+
+
+
+
+SELECT 
+*
+,case when val like '%(%' then 1 else 0 end as predok
+,case when val like '%)%' then 1 else 0 end as zak
+,case when val like '%select%' then 1 else 0 end as select_
+,case when val like '%FROM%' then 1 else 0 end as from_
+,case when val like '%join%' then 1 else 0 end as join_
+,case when val like '%where%' then 1 else 0 end as where_
+FROM TK_TEST2
+
+
+
+
+
+
+-- @token = @tokenen
+-- @token_i = @tokenen_i
+
+DECLARE @table TABLE (tik varchar(100), tok varchar(100), order_ INT)
+DECLARE @token_i VARCHAR(100) = ''
+DECLARE @get_next BIT = 0 -- FALSE (1 = TRUE)
+DECLARE @previous VARCHAR(100) = ''
+DECLARE @order INT = 1
+DECLARE @previous_tik VARCHAR(100) = ''
+DECLARE @previous_get BIT = 0 -- FALSE
+
+DECLARE @ttok VARCHAR(100) = ''
+
+
+DECLARE @i_row INT = 1
+DECLARE @max_row INT = (SELECT MAX(rn) FROM TK_TEST2)
+DECLARE @row_commands_1 NVARCHAR(1000) = 'select,delete,insert,drop,create,select,truncate,exec,execute'
+DECLARE @row_commands_2 NVARCHAR(1000) = 'select,not,if,exists,select'
+DECLARE @row_commands_3 NVARCHAR(1000) = 'from,join,into,table,exists,sys.dm_exec_sql,exec,execute'
+
+
+
+WHILE (@max_row >= @i_row)
+BEGIN
+		DECLARE @token VARCHAR(1000) = (SELECT val FROM TK_TEST2 WHERE rn = @i_row)
+
+			IF @token IN (SELECT REPLACE(TRIM(LOWER(value)), ' ','') FROM STRING_SPLIT(@row_commands_1, ','))
+			BEGIN
+				IF LOWER(@token) = 'select'
+					BEGIN
+						SET @token = 'select'
+					END
+				SET @token_i = @token
+			END
+			IF (@get_next = 1)
+			BEGIN
+					IF @token NOT IN (SELECT REPLACE(TRIM(LOWER(value)), ' ',' ') FROM STRING_SPLIT(@row_commands_2,','))
+					BEGIN
+						IF (LOWER(@previous) = 'into')
+							SET @token_i = 'select into'
+						IF (@token NOT LIKE '%#%' OR @token NOT LIKE '%#%')
+						
+								SET @ttok = ' ' + @token + ' as ('
+								--IF (@ttok NOT IN (SELECT @token))
+								 IF (@ttok NOT IN (SELECT @stmt2))
+									INSERT INTO @table (tik, tok, order_)
+									SELECT @token_i, @token, @order
+
+						SET @token_i = @token_i
+					END
+					SET @get_next = 0
+					IF @token = 'sys.dm_exec_sql_text'
+					BEGIN
+						SET @get_next = 1 
+					END
+			END
+			IF (@token IN (SELECT REPLACE(TRIM(LOWER(value)), ' ','') FROM STRING_SPLIT(@row_commands_3,',')))
+			BEGIN
+				SET @get_next = 1
+			END
+
+			SET @previous_tik  = @token_i
+			SET @previous = @token							
+
+			SET @i_row = @i_row + 1
+END
+
+-- Final results
+SELECT *, row_number() over (order by (select 1)) as rn FROM @table
 
 
 
@@ -205,3 +345,38 @@ BEGIN
 -- END of procedure
 END;
 GO
+
+
+
+
+
+-- TEST
+
+declare @test_query VARCHAR(8000) = '
+
+SELECT 
+    s.[BusinessEntityID]
+    ,p.[Title]
+    ,p.[FirstName]
+    ,p.[MiddleName]
+    ,p.[LastName]
+    ,p.[Suffix]
+    ,e.[JobTitle] as imeSluzbe
+    ,p.[EmailPromotion]
+    ,s.[SalesQuota]
+    ,s.[SalesYTD]
+    ,s.[SalesLastYear]
+	,( SELECT GETDATE() ) AS DateNow
+	,( select count(*)  FROM [AdventureWorks2014].sales.[SalesPerson] ) as totalSales
+
+FROM [AdventureWorks2014].sales.[SalesPerson] s
+    LEFT JOIN [AdventureWorks2014].[HumanResources].[Employee] e 
+    ON e.[BusinessEntityID] = s.[BusinessEntityID]
+	INNER JOIN [AdventureWorks2014].[Person].[Person] AS p
+	ON p.[BusinessEntityID] = s.[BusinessEntityID]
+
+'
+
+
+EXEC dbo.TSQL_data_lineage 
+  @InputQuery = @test_query
